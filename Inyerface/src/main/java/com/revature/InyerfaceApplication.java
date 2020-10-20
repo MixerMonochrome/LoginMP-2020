c63e6d8eb35da49a2c992ae81a6a6351effb1698
Tanisi90 <furalpanda@yahoo.com>
2020-10-20 10:39:20 -0700

    basic

---
+++ Inyerface/src/main/java/com/revature/InyerfaceApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyerfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyerfaceApplication.class, args);
+	}
+
+}
